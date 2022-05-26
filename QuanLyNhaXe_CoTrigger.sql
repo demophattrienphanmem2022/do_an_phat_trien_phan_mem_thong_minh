6b503590c0ac44c9ff5d37980051af78e93d40cb
tinhph1108 <mom11082001@gmail.com>
2022-05-26 07:08:09 +0700

    05/26/2022

--- QuanLyNhaXe_CoTrigger.sql
+++ QuanLyNhaXe_CoTrigger.sql
@@ -15,21 +15,22 @@ create table Identify
 	LoTrinh int default 0,
 	Xe int default 0,
 	LichChay int default 0,
-	VeXe int default 0
+	VeXe int default 0,
+	LichChay_Xe int default 0
 )
 go
 create table LoaiNhanVien
 (
 	MaLoaiNhanVien int identity(1,1) not null primary key,
 	TenLoaiNhanVien nvarchar(100) not null,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 )
 go
 create table LoaiTaiKhoan
 (
 	MaLoaiTaiKhoan int identity(1,1) not null primary key,
 	TenLoaiTaiKhoan nvarchar(100) not null,
-	fl_Xoa bit not null
+	fl_Xoa int not null
 )
 go
 create table TaiKhoan
@@ -39,7 +40,7 @@ create table TaiKhoan
 	MaLoaiTaiKhoan int not null,
 	fl_NgayThem datetime not null,
 	fl_NgaySua datetime,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 	CONSTRAINT FK_TaiKhoan_LoaiTaiKhoan FOREIGN KEY (MaLoaiTaiKhoan) REFERENCES LoaiTaiKhoan(MaLoaiTaiKhoan)
 )
 go
@@ -53,10 +54,10 @@ create table NhanVien
 	CCCD varchar(15) not null,
 	DiaChi nvarchar(500) not null,
 	MaLoaiNhanVien int not null,
-	TenDangNhap varchar(50) not null unique,
+	TenDangNhap varchar(50) unique,
 	fl_NgayThem datetime not null,
 	fl_NgaySua datetime,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 	CONSTRAINT FK_NhanVien_LoaiNhanVien FOREIGN KEY (MaLoaiNhanVien) REFERENCES LoaiNhanVien(MaLoaiNhanVien),
 	CONSTRAINT FK_NhanVien_TaiKhoan FOREIGN KEY (TenDangNhap) REFERENCES TaiKhoan(TenDangNhap)
 )
@@ -65,15 +66,15 @@ create table KhachHang
 (
 	MaKhachHang varchar(10) primary key,
 	TenKhachHang nvarchar(50) not null,
-	GioiTinh nvarchar(3) not null,
-	NgaySinh Datetime not null,
+	GioiTinh nvarchar(3),
+	NgaySinh Datetime,
 	DienThoai varchar(10) not null,
-	CCCD varchar(15) not null,
-	DiaChi nvarchar(500) not null,
-	TenDangNhap varchar(50) not null,
+	CCCD varchar(15),
+	DiaChi nvarchar(500),
+	TenDangNhap varchar(50),
 	fl_NgayThem datetime not null,
 	fl_NgaySua datetime,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 	CONSTRAINT FK_KhachHang_TaiKhoan FOREIGN KEY (TenDangNhap) REFERENCES TaiKhoan(TenDangNhap)
 )
 go
@@ -81,7 +82,7 @@ create table LoaiXe
 (
 	MaLoaiXe int identity(1,1) not null primary key,
 	TenLoaiXe nvarchar(100) not null,
-	fl_Xoa bit not null
+	fl_Xoa int not null
 )
 go
 create table Xe
@@ -90,7 +91,7 @@ create table Xe
 	BienSo varchar(20) not null,
 	SoGhe int not null,
 	MaLoaiXe int not null,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 	CONSTRAINT FK_Xe_LoaiXe FOREIGN KEY (MaLoaiXe) REFERENCES LoaiXe(MaLoaiXe)
 )
 go
@@ -103,7 +104,7 @@ create table LoTrinh
 	GiaVe int not null,
 	fl_NgayThem datetime not null,
 	fl_NgaySua datetime,
-	fl_Xoa bit not null
+	fl_Xoa int not null
 )
 go
 create table LichChay
@@ -115,7 +116,7 @@ create table LichChay
 	MaNhanVien varchar(10) not null,
 	fl_NgayThem datetime not null,
 	fl_NgaySua datetime,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 	CONSTRAINT FK_LichChay_LoTrinh FOREIGN KEY (MaLoTrinh) REFERENCES LoTrinh(MaLoTrinh),
 	CONSTRAINT FK_LichChay_NhanVien FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNhanVien)
 )
@@ -123,12 +124,12 @@ go
 create table LichChay_Xe 
 (
 	MaLichChay_Xe varchar(10) not null primary key,
-	TrangThai bit not null, --0 là chưa khởi hành, 1 là xe đã khởi hành
+	TrangThai int not null, --0 là chưa khởi hành, 1 là xe đã khởi hành
 	MaLichChay varchar(10) not null,
 	MaXe varchar(10) not null,
 	fl_NgayThem datetime not null,
 	fl_NgaySua datetime,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 	CONSTRAINT FK_LichChay_Xe_LichChay FOREIGN KEY (MaLichChay) REFERENCES LichChay(MaLichChay),
 	CONSTRAINT FK_LichChay_Xe_Xe FOREIGN KEY (MaXe) REFERENCES Xe(MaXe)
 )
@@ -137,7 +138,7 @@ create table DiaChiTrungChuyen
 (
 	MaDiaChiTrungChuyen varchar(10) not null primary key,
 	DiaChi nvarchar(100) not null,
-	fl_Xoa bit not null
+	fl_Xoa int not null
 )
 go
 create table TrungChuyen
@@ -149,7 +150,7 @@ create table TrungChuyen
 	MaDiaChiTrungChuyen varchar(10) not null,
 	fl_NgayThem datetime not null,
 	fl_NgaySua datetime,
-	fl_Xoa bit not null,
+	fl_Xoa int not null,
 	CONSTRAINT FK_TrungChuyen_DiaChiTrungChuyen FOREIGN KEY (MaDiaChiTrungChuyen) REFERENCES DiaChiTrungChuyen(MaDiaChiTrungChuyen)
 )
 go
@@ -162,8 +163,8 @@ create table VeXe
 	MaNhanVien varchar(10) not null,
 	MaKhachHang varchar(10) not null,
 	MaLichChay_Xe varchar(10) not null,
-	MaTrungChuyen varchar(10) not null,
-	fl_Xoa bit not null,
+	MaTrungChuyen varchar(10),
+	fl_Xoa int not null,
 	CONSTRAINT FK_VeXe_KhachHang FOREIGN KEY (MaKhachHang) REFERENCES KhachHang(MaKhachHang),
 	CONSTRAINT FK_VeXe_NhanVien FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNhanVien),
 	CONSTRAINT FK_VeXe_LichChay_Xe FOREIGN KEY (MaLichChay_Xe) REFERENCES LichChay_Xe(MaLichChay_Xe),
@@ -173,7 +174,7 @@ go
 create table ChiTietVeXe
 (
 	MaVeXe varchar(10) not null,
-	GheNgoi int not null,
+	GheNgoi varchar(5) not null,
 	CONSTRAINT pk_ChiTietVeXe primary key(MaVeXe, GheNgoi),
 	CONSTRAINT FK_ChiTietVeXe_VeXe FOREIGN KEY (MaVeXe) REFERENCES VeXe(MaVeXe)
 )
@@ -219,6 +220,14 @@ BEGIN
 END
 GO
 
+CREATE TRIGGER trg_CountLichChay ON LichChay_Xe
+AFTER INSERT
+AS
+BEGIN
+    UPDATE Identify SET LichChay_Xe = LichChay_Xe+1
+END
+GO
+
 CREATE TRIGGER trg_CountVeXe ON VeXe
 AFTER INSERT
 AS
@@ -227,6 +236,19 @@ BEGIN
 END
 GO
 
+CREATE TRIGGER trg_UpdateVeXe ON ChiTietVeXe
+AFTER DELETE
+AS
+BEGIN
+	if((select VeXe.SoLuong from VeXe where VeXe.MaVeXe = (select deleted.MaVeXe from deleted))>1)
+		UPDATE VeXe
+		set SoLuong = SoLuong - 1, ThanhTien = ThanhTien - DonGia
+		where VeXe.MaVeXe = (select deleted.MaVeXe from deleted)
+	else
+		UPDATE VeXe
+		set SoLuong = SoLuong - 1, ThanhTien = ThanhTien - DonGia, fl_Xoa = 1
+		where VeXe.MaVeXe = (select deleted.MaVeXe from deleted)
+END
 ---------------Nhập dữ liệu loại nhân viên
 ----------------Indentity
 go
@@ -264,13 +286,13 @@ values(N'Xe trung chuyển',0)
 ---------------Nhập tài khoản
 go
 insert into TaiKhoan
-values('NV00001','123',1,'09/05/2022',NULL,0)
+values('NV00001','123',1,'05/09/2022',NULL,0)
 go
 insert into TaiKhoan
-values('NV00002','123',2,'09/05/2022',NULL,0)
+values('NV00002','123',2,'05/09/2022',NULL,0)
 go
 insert into TaiKhoan
-values('KH00001','123',3,'09/05/2022',NULL,0)
+values('KH00001','123',3,'05/09/2022',NULL,0)
 ---------------Nhập nhân viên
 go
 insert into NhanVien
@@ -278,8 +300,106 @@ values('NV00001',N'Phạm Hữu Tính',N'Nam','08/11/2001','0375075701','6604564
 go
 insert into NhanVien
 values('NV00002',N'Nguyễn Cẩm Lê',N'Nữ','01/19/2001','0939989999','940565656232',N'Đình Thành, Đông Hải, TP. Bạc Liêu',2,'NV00002','05/09/2022',NULL,0)
+go
+insert into NhanVien
+values('NV00003',N'Trần Trọng',N'Nam','01/01/2001','0562134587','558741254875',N'TP. Hồ Chí Minh',3,NULL,'05/09/2022',NULL,0)
 ---------------Nhập khách hàng
 go
 insert into KhachHang
 values('KH00001',N'Lê Huỳnh Nam',N'Nam','01/10/2001','0856123456','540521541236',N'Quận 6, TP. Hồ Chí Minh','KH00001','05/09/2022',NULL,0)
+--------------Nhập lộ trình
 go
+insert into LoTrinh
+values('LT00001',N'Hồ Chí Minh',N'Long An',30,50000,'05/09/2022',NULL,0)
+go
+insert into LoTrinh
+values('LT00002',N'Hồ Chí Minh',N'Tiền Giang',60,75000,'05/09/2022',NULL,0)
+go
+insert into LoTrinh
+values('LT00003',N'Hồ Chí Minh',N'Sa Đéc',170,100000,'05/09/2022',NULL,0)
+go
+insert into LoTrinh
+values('LT00004',N'Hồ Chí Minh',N'Đồng Tháp',200,150000,'05/09/2022',NULL,0)
+------------Nhập xe
+go
+insert into Xe
+values('XE00001','51B00001',46,1,0)
+go
+insert into Xe
+values('XE00002','51B00002',46,1,0)
+go
+insert into Xe
+values('XE00003','51B00003',46,1,0)
+go
+insert into Xe
+values('XE00004','51B00004',46,1,0)
+go
+insert into Xe
+values('XE00005','51B00005',46,1,0)
+go
+insert into Xe
+values('XE00006','51B00006',46,1,0)
+go
+insert into Xe
+values('XE00007','51B00007',46,1,0)
+go
+insert into Xe
+values('XE00008','51B00008',46,1,0)
+go
+insert into Xe
+values('XE00009','51B00009',46,1,0)
+go
+insert into Xe
+values('XE00010','51B00010',46,1,0)
+go
+insert into Xe
+values('XE00011','51B00011',16,2,0)
+go
+insert into Xe
+values('XE00012','51B00012',16,2,0)
+go
+insert into Xe
+values('XE00013','51B00013',16,2,0)
+go
+insert into Xe
+values('XE00014','51B00014',16,2,0)
+go
+insert into Xe
+values('XE00015','51B00015',16,2,0)
+-------------Nhập lịch chạy
+go
+insert into LichChay
+values('LC00001','05/25/2022','19','LT00001','NV00003','05/09/2022',NULL,0)
+go
+insert into LichChay
+values('LC00002','05/25/2022','19','LT00002','NV00003','05/09/2022',NULL,0)
+--------------Nhập lịch chạy xe
+go
+insert into LichChay_Xe
+values('LCX00001',0,'LC00001','XE00001','05/09/2022',NULL,0)
+go
+insert into LichChay_Xe
+values('LCX00002',0,'LC00001','XE00002','05/09/2022',NULL,0)
+go
+insert into LichChay_Xe
+values('LCX00003',0,'LC00002','XE00003','05/09/2022',NULL,0)
+go
+insert into LichChay_Xe
+values('LCX00004',0,'LC00002','XE00004','05/09/2022',NULL,0)
+-------------Nhập vé xe
+go
+insert into VeXe
+values('VX00001',50000,2,100000,'NV00002','KH00001','LCX00001',NULL,0)
+------------Nhập chi tiết vé xe
+go
+insert into ChiTietVeXe
+values('VX00001','01A')
+go
+insert into ChiTietVeXe
+values('VX00001','02A')
+
+select * from LichChay
+
+select lc.MaLichChay, lc.NgayKhoiHanh, lc.GioKhoiHanh, lc.MaLoTrinh, nv.TenNhanVien, lc.fl_NgayThem, lc.fl_NgaySua
+from LichChay lc, NhanVien nv
+where lc.MaNhanVien = nv.MaNhanVien
